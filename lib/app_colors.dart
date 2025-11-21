431018e404295210d2c086f28199dcf01c2ab001
DESKTOP-4DH72C6\Huessin Sobhy <mohamedyamany055@gmail.com>
2025-11-21 19:26:41 +0200

    add app color file

---
+++ lib/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color activeCardColour = Color(0xFF1D1E33);
+  static const Color inActiveCardColour = Color(0xFF111328);
+  static const Color primaryColor = Color(0xFFEB1555);
+  static const Color grey = Color(0xFF8D8E98);
+  static const Color backGround = Color(0xFF0A0E21);
+}
